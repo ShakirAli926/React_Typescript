3e9860c8d916f5aceacebec8dbae9e68fb812a67
ShakirAli926 <shakirbugti926@gmail.com>
2024-02-07 16:12:47 +0500

    fix the undefined css error because of optional parameter of props.

--- src/components/CourseGoalsList.tsx
+++ src/components/CourseGoalsList.tsx
@@ -21,7 +21,7 @@ export default function CourseGoalsList({
 
   if (goals.length >= 4) {
     warningBox = (
-      <InfoBox mode="warning">
+      <InfoBox mode="warning" serverity="high">
         You' re collecting a lot of goals. Don't put too much on your plate!
       </InfoBox>
     );
